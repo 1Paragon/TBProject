b9c0fbcd09a8d93bc517829f6d8ee4e8785ea014
1Paragon <1Paragon@protonmail.com>
2020-09-27 11:52:22 -0700

    added week2and3 homework to repo

---
+++ HomeworkWeek2/week2.js
@@ -0,0 +1,22 @@
+let pizzaPlace = "Pizza Pie";
+let numberOfToppings = 10;
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
+console.log(pizzaPlace);
+console.log(numberOfToppings);
+let menu = `Welcome to " + pizzaPlace + ", You can pick from" + numberOfToppings`;
+console.log(menu);
+ let choice = 1
+ If numberOfToppings <10 {
+     return = "Quality, not quantity";
+ } else {
+     numberOfToppings =>10
+     return = "A Whole lot of Pizza";
+console.log("Quality, not quantity", "A Whole lot of Pizza");
+ //BONUS
+let numbers = [0,1,...10];
+ let sum = 0
+  for (let x=0; x<numbers.length; x++) {
+     sum = numbers[x]+2 
+ }
+console.log(sum);
