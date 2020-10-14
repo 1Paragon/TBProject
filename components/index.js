8343af8eac67f1fa166c42042f2786777ae85cd1
1Paragon <1Paragon@protonmail.com>
2020-10-13 22:31:34 -0700

    SPA conversion

--- components/index.js
+++ components/index.js
@@ -1,5 +1,8 @@
 //export { default as Header } from "./header"
 //I think I need one of these for each file in components folder
 //except the js one, which i'm in
-
-export { default as Blog } from "./Blog";export { default as Blog } from "./Blog";
+export { default as header } from "./header";
+export { default as footer } from "./footer";
+export { default as main } from "./main";
+export { default as nav } from "./nav";
+export { default as blog } from "./blog";
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 console.log(document.getElementsByTagName("ul"));
 console.log(document.querySelectorAll("ul"));
+//Add event listeners here after rendering
 
 import axios from "axios";
 axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
