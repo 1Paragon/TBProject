2329c9c04b4012065ad676a525242cf7f591fb62
1Paragon <1Paragon@protonmail.com>
2020-10-13 20:08:22 -0700

    more

--- components/footer.js
+++ components/footer.js
@@ -1,1 +1,7 @@
-xxx;
+export default() => `${<footer>
+  &copy; 2020 <a href="https://savvycoders.com/">Savvy Coders</a>
+</footer>
+<script src="index.js"></script>
+</body>
+</html>
+}`
--- components/index.js
+++ components/index.js
@@ -1,1 +1,5 @@
-xxxx;
+//export { default as Header } from "./header"
+//I think I need one of these for each file in components folder
+//except the js one, which i'm in
+
+export { default as Blog } from "./Blog";export { default as Blog } from "./Blog";
