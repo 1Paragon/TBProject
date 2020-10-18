55a09020af891b2a122b2f640dc2c6f4e1cb3339
1Paragon <1Paragon@protonmail.com>
2020-10-18 15:06:36 -0700

    updates

--- components/nav.js
+++ components/nav.js
@@ -10,20 +10,18 @@ export default() => `${
 <nav>
   <i class="fas fa-bars"></i>
   <ul class="hidden--mobile nav-links">
-  <li>Home</li>
-       <li>Join Us</li>
-       <li>Happenings</li>
-       <li>Keep in Touch</li>
-       <li>About Us</li>
-       <li>Administration</li>
+  <li><a href="index.html>Home<a/></li>
+  <li><a href="header.html>Join Us</a></li>
+  <li><a heref="main.html">Happenings</a></li>
+ <li><a heref="main.html">Keep in Touch</a></li>
+ <li><a heref="main.html">About Us</a></li>
+ <li><a heref="main.html">Administration</a></li>
 
     ${linkSync.reduce( html,link) => html +
 //one of these entered for each of the nav files in views
     `<li><a href="/${bio.html !== "Home" ? Bio.html : "}" title="${
       Bio.html
     }" data-navigo>${bio.text}</a></li>`,
-
-
 
    //relative to views files
     <li><a href= "gallery.html">Gallery</a></li>
