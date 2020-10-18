b790337a86c9eacbb08b979733aa8df061d36992
1Paragon <1Paragon@protonmail.com>
2020-10-18 15:28:22 -0700

    +

--- components/nav.js
+++ components/nav.js
@@ -12,10 +12,10 @@ export default() => `${
   <ul class="hidden--mobile nav-links">
   <li><a href="index.html>Home<a/></li>
   <li><a href="header.html>Join Us</a></li>
-  <li><a heref="main.html">Happenings</a></li>
- <li><a heref="main.html">Keep in Touch</a></li>
- <li><a heref="main.html">About Us</a></li>
- <li><a heref="main.html">Administration</a></li>
+  <li><a href="main.html">Happenings</a></li>
+ <li><a href="main.html">Keep in Touch</a></li>
+ <li><a href="main.html">About Us</a></li>
+ <li><a href="main.html">Administration</a></li>
 
     ${linkSync.reduce( html,link) => html +
 //one of these entered for each of the nav files in views
@@ -23,13 +23,6 @@ export default() => `${
       Bio.html
     }" data-navigo>${bio.text}</a></li>`,
 
-   //relative to views files
-    <li><a href= "gallery.html">Gallery</a></li>
-    <li><a href="register.html">Register</a></li>
-
-
-    //all ends with
-    )}
+   )};
   </ul>
 </nav>
-`;
