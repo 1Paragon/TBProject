75f5eea8612db24f6f2fff3a220a79a3f107e0a3
1Paragon <1Paragon@protonmail.com>
2020-11-02 21:04:18 -0800

    css updates

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,7 +1,7 @@
 export default st => `
 <section id="jumbotron">
- <img src="https://github.com/1Paragon/TBProject/blob/master/Photos/river%20edge%20.jpg?raw=true"
- alt="river edge">
+ <img src="https://github.com/1Paragon/TBProject/blob/master/Photos/river%20edge1e%20.jpg?raw=true"
+  alt="river edge">
   <h2>Helping form future tech leaders</h2>
   <h3>Supporting youth as they utilize technology
   <br>to navigate our ever changing world</h3>
