5e1d447ceeaefad2c4e831cb1ccc5c6f9bc406d4
1Paragon <1Paragon@protonmail.com>
2020-11-05 19:17:54 -0800

    fix

--- components/views/gallery.js
+++ components/views/gallery.js
@@ -1,6 +1,7 @@
 export default st => `
 ${st.Gallery.reduce(
   (html, Photogallery) =>
-    html + `<img src="${Photogallery.url}" alt="${Photogallery.title}">`,
+    html +
+    `<img src="${Photogallery.url}" alt="${Photogallery.title}width="200" height="150">`,
   ``
 )}`;
