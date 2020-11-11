68f1fc33b0e498f23b25964b5640bbf9312b8a06
1Paragon <1Paragon@protonmail.com>
2020-11-10 20:30:29 -0800

    update

--- components/views/Blog.js
+++ components/views/Blog.js
@@ -1,5 +1,11 @@
 export default st => `
 <section id="Blog">
+<h4>If you would like to
+    keep in touch and stay
+    informed about Paragon
+    Organization's latest happenings</h4>
+<h3>Please check out our Blog</h3>
+
 ${st.posts
   .map(post => {
     formatBlogPost(post);
